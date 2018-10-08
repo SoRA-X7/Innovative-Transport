54faf2a0c6a9b4133829b50125e61e5955dd82fe
kitihi20 <kirihatah@gmail.com>
2018-10-08 20:23:19 +0900

    デバッガーslot対応

--- src/main/java/solab/innovativetransport/Debugger.java
+++ src/main/java/solab/innovativetransport/Debugger.java
@@ -1,5 +1,6 @@
 package solab.innovativetransport;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -11,6 +12,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+import solab.innovativetransport.pipe.EnumConnectionType;
 import solab.innovativetransport.pipe.TilePipe;
 
 /**
@@ -138,13 +140,28 @@ public class Debugger extends Item {
                 ctex="§a";
             }
         }
-        if (mode == 2) {
-            if(world.getBlockState(pos).getBlock() == InnovativeTransport.blocks[0]){
-                mc.thePlayer.addChatMessage(new TextComponentTranslation("x"));
-            }
-
-        }
         return EnumActionResult.PASS;
+    }
+    @Override
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        IBlockState blockState = worldIn.getBlockState(pos);
+        if (mode == 2) {
+            if (blockState.getBlock() == InnovativeTransport.blocks[0]) {
+                if (blockState.getValue(TilePipe.states.get(facing)) != EnumConnectionType.none) {
+                    Minecraft mc = Minecraft.getMinecraft();
+                    if(worldIn.isRemote){
+                        mc.thePlayer.addChatMessage(new TextComponentTranslation("§aCLIENT-----------------"));
+                        mc.thePlayer.addChatMessage(new TextComponentTranslation("§a"+blockState.getValue(TilePipe.states.get(facing)).toString()));
+                    }else if(!worldIn.isRemote){
+                        mc.thePlayer.addChatMessage(new TextComponentTranslation("§cSERVER-----------------"));
+                        mc.thePlayer.addChatMessage(new TextComponentTranslation("§c"+blockState.getValue(TilePipe.states.get(facing)).toString()));
+                        mc.thePlayer.addChatMessage(new TextComponentTranslation("-----------------------"));
+                    }
+                    return EnumActionResult.SUCCESS;
+                }
+            }
+        }
+        return EnumActionResult.FAIL;
     }
 
 }
