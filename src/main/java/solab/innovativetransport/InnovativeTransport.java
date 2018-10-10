2088f06a95eee6dc1e5b7de2cf31e5c485f0128e
GALAXYCOMET2002 <galaxycomet2002@gmail.com>
2018-10-10 21:50:34 +0900

    意味不（意味不）

--- src/main/java/solab/innovativetransport/InnovativeTransport.java
+++ src/main/java/solab/innovativetransport/InnovativeTransport.java
@@ -4,12 +4,14 @@ import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import solab.innovativetransport.card.ItemCardSlot;
 import solab.innovativetransport.pipe.BlockPipe;
+import solab.innovativetransport.pipe.PipeSpecialRenderer;
 import solab.innovativetransport.pipe.TilePipe;
 
 @Mod(modid = InnovativeTransport.MODID)
@@ -35,5 +37,8 @@ public class InnovativeTransport {
         InnovativeTransportRegisterBlocks.registerBlocks(blocks,event.getSide().isClient());
         GameRegistry.registerTileEntity(TilePipe.class,MODID + ":transportpipe");
         InnovativeTransportRegisterItems.registerItems(items,event.getSide().isClient());
+        if (event.getSide().isClient()) {
+            ClientRegistry.bindTileEntitySpecialRenderer(TilePipe.class, new PipeSpecialRenderer());
+        }
     }
 }
--- src/main/java/solab/innovativetransport/pipe/PipeSpecialRenderer.java
+++ src/main/java/solab/innovativetransport/pipe/PipeSpecialRenderer.java
@@ -1,9 +1,20 @@
 package solab.innovativetransport.pipe;
 
+import com.google.common.base.Function;
+import net.minecraft.client.renderer.BlockModelRenderer;
 import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.client.model.animation.FastTESR;
 import org.lwjgl.opengl.GL11;
+import solab.innovativetransport.InnovativeTransport;
 import solab.innovativetransport.transporter.ItemTransporter;
+
+import javax.annotation.Nullable;
 
 public class PipeSpecialRenderer extends FastTESR<TilePipe> {
 
@@ -13,6 +24,25 @@ public class PipeSpecialRenderer extends FastTESR<TilePipe> {
             TilePipe pipe = (TilePipe)te;
             if (pipe.pipe == null) return;
 
+            //Render Pipe
+            IBakedModel bakedModel;
+            try {
+                bakedModel = ModelLoaderRegistry.getModel(new ResourceLocation(InnovativeTransport.MODID,"pipe_side_off")).bake(null, new VertexFormat(), new Function<ResourceLocation, TextureAtlasSprite>() {
+                    @Nullable
+                    @Override
+                    public TextureAtlasSprite apply(@Nullable ResourceLocation input) {
+                        return null;
+                    }
+                });
+
+                BlockModelRenderer renderer = new BlockModelRenderer(BlockColors.init());
+                renderer.renderModel(te.getWorld(),bakedModel,te.getWorld().getBlockState(te.getPos()),te.getPos(),VertexBuffer,false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //Render Items/Fluids
+            renderSolids(pipe.pipe,x,y,z,partialTicks);
         }
     }
 
